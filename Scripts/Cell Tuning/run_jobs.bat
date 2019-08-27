a17ab8124e4a3aed83b2eea73a2336a160aca61e
Harsha Gurnani <harsha.gurnani.16@ucl.ac.uk>
2019-08-27 09:01:01 +0100

    Update run_jobs.bat

--- Scripts/Cell Tuning/run_jobs.bat
+++ Scripts/Cell Tuning/run_jobs.bat
@@ -1,4 +1,11 @@
 set root=C:\Users\Harsha\Anaconda3
 call %root%\Scripts\activate.bat %root%
 call conda activate nml2
-python LEMS_sim_gocnet_GoC_0_nrn.py
+
+FOR /L %%A IN (1,1,2) DO(
+  python run_varied_GoC_single.py %%A
+)
+for %%f in (LEMS_sim_gocnet_GoC*.py) do (
+    echo %%~nf
+	python "%%~nf.py"
+)
