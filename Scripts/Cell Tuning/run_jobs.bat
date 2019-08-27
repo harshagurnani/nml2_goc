6c23773d10a7011dabe30cfccc0baafd0f7ee235
Harsha@work <harsha.gurnani.16@ucl.ac.uk>
2019-08-27 08:52:45 +0100

    Update run_jobs.bat

--- Scripts/Cell Tuning/run_jobs.bat
+++ Scripts/Cell Tuning/run_jobs.bat
@@ -1,4 +1,4 @@
-set root=C:\Users\%USERNAME%\Miniconda3_32
+set root=C:\Users\Harsha\Anaconda3
 call %root%\Scripts\activate.bat %root%
 call conda activate nml2
-python LEMS_sim_
+python LEMS_sim_gocnet_GoC_0_nrn.py
